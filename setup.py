7bf95af55a5b762e8f4b076862621041457c430a
Tulir Asokan <tulir@maunium.net>
2019-05-01 14:23:00 +0300

    Add ruamel.yaml to dependencies

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
     install_requires=[
         "aiohttp>=3.0.1,<4",
         "mautrix>=0.4.0.dev31,<0.5.0",
+        "ruamel.yaml>=0.15.94,<0.16",
         "fbchat-asyncio>=0.1.1,<0.2.0",
         "SQLAlchemy>=1.2,<2",
         "alembic>=1,<2",
