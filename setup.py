9f2b1d16ac74b14539c5527a056dd865082935be
Tulir Asokan <tulir@maunium.net>
2019-11-21 20:20:58 +0200

    Fix fbchat-asyncio dep

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         "ruamel.yaml>=0.15.94,<0.17",
         "commonmark>=0.8,<0.10",
         "python-magic>=0.4,<0.5",
-        "fbchat-asyncio>=0.3.0.beta1",
+        "fbchat-asyncio>=0.3.0.beta2",
         "SQLAlchemy>=1.2,<2",
         "alembic>=1,<2",
     ],
