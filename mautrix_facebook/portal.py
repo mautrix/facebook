18f4139e616a77c99d3af4121da1c34ebc92588b
Tulir Asokan <tulir@maunium.net>
2020-06-15 16:42:51 +0300

    Don't try to send m.bridge events before portal is created

--- mautrix_facebook/portal.py
+++ mautrix_facebook/portal.py
@@ -391,6 +391,9 @@ class Portal(BasePortal):
         }
 
     async def update_bridge_info(self) -> None:
+        if not self.mxid:
+            self.log.debug("Not updating bridge info: no Matrix room created")
+            return
         try:
             self.log.debug("Updating bridge info...")
             await self.main_intent.send_state_event(self.mxid, StateBridge,
