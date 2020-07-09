38fbf4fe65466f100c900858e661805cbafa20a4
Tulir Asokan <tulir@maunium.net>
2020-07-09 16:57:52 +0300

    Update mautrix-python

--- mautrix_facebook/db/__init__.py
+++ mautrix_facebook/db/__init__.py
@@ -8,11 +8,6 @@ from .user import User
 from .user_portal import UserPortal
 from .user_contact import Contact
 
-try:
-    from mautrix.bridge.db.nio_state_store import init as init_nio_db
-except ImportError:
-    init_nio_db = None
-
 
 def init(db_engine) -> None:
     for table in (Portal, Message, Reaction, User, Puppet, UserPortal, Contact, UserProfile,
@@ -21,5 +16,3 @@ def init(db_engine) -> None:
         table.t = table.__table__
         table.c = table.t.c
         table.column_names = table.c.keys()
-    if init_nio_db:
-        init_nio_db(db_engine)
