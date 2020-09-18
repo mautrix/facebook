d6d7cb54869b3300fba54eddd45322e13cfcbf85
Tulir Asokan <tulir@maunium.net>
2020-09-18 17:46:11 +0300

    Remove entry_points and 3.6 from setup.py

--- setup.py
+++ setup.py
@@ -57,14 +57,9 @@ setuptools.setup(
         "Framework :: AsyncIO",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    entry_points="""
-        [console_scripts]
-        mautrix-facebook=mautrix_facebook.__main__:main
-    """,
     package_data={"mautrix_facebook": [
         "web/static/*",
         "example-config.yaml",
