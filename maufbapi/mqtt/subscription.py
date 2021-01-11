068111515c3e9be5cd70b5a66efabcd13b9f2f47
Tulir Asokan <tulir@maunium.net>
2021-01-11 20:54:11 +0200

    Add full encoded topic map

--- maufbapi/mqtt/subscription.py
+++ maufbapi/mqtt/subscription.py
@@ -15,47 +15,19 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from typing import Dict
 from enum import Enum
+import pkgutil
+import json
 
-
-_topic_map: Dict[str, str] = {
-    "/messenger_sync_create_queue": "23",
-    "/t_ms": "59",
-
-    "/pp": "34",  # unknown
-    "/ig_sub_iris": "134",
-    "/ig_sub_iris_response": "135",
-    "/ig_message_sync": "146",
-    "/ig_send_message": "132",
-    "/ig_send_message_response": "133",
-    "/ig_realtime_sub": "149",
-    "/pubsub": "88",
-    "/t_fs": "102",  # Foreground state
-    "/graphql": "9",
-    "/t_region_hint": "150",
-    "/mqtt_health_stats": "/mqtt_health_stats",
-    "179": "179",  # also unknown
-}
-
+_raw_topic_map: Dict[str, int] = json.loads(pkgutil.get_data("maufbapi.mqtt", "topics.json"))
+# Mapping from name to numeric ID
+_topic_map: Dict[str, str] = {key: str(value) for key, value in _raw_topic_map.items()}
+# Mapping from numeric ID to name
 _reverse_topic_map: Dict[str, str] = {value: key for key, value in _topic_map.items()}
 
 
 class RealtimeTopic(Enum):
     SYNC_CREATE_QUEUE = "/messenger_sync_create_queue"
     T_MS = "/t_ms"
-
-    SUB_IRIS = "/ig_sub_iris"
-    SUB_IRIS_RESPONSE = "/ig_sub_iris_response"
-    MESSAGE_SYNC = "/ig_message_sync"
-    SEND_MESSAGE = "/ig_send_message"
-    SEND_MESSAGE_RESPONSE = "/ig_send_message_response"
-    REALTIME_SUB = "/ig_realtime_sub"
-    PUBSUB = "/pubsub"
-    FOREGROUND_STATE = "/t_fs"
-    GRAPHQL = "/graphql"
-    REGION_HINT = "/t_region_hint"
-    MQTT_HEALTH_STATS = "/mqtt_health_stats"
-    UNKNOWN_PP = "/pp"
-    UNKNOWN_179 = "179"
 
     @property
     def encoded(self) -> str:
