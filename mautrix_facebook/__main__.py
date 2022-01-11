b133da98820434a9e9bd73d8251660e019bce4be
Tulir Asokan <tulir@maunium.net>
2022-01-11 16:23:18 +0200

    Fix reconnection error log. Closes #202

--- mautrix_facebook/__main__.py
+++ mautrix_facebook/__main__.py
@@ -145,7 +145,7 @@ class MessengerBridge(Bridge):
                     log.debug("Periodic reconnect loop stopped")
                     return
                 except Exception:
-                    log.exception("Error while reconnecting", user.mxid)
+                    log.exception("Error while reconnecting %s", user.mxid)
 
     async def get_portal(self, room_id: RoomID) -> Portal:
         return await Portal.get_by_mxid(room_id)
