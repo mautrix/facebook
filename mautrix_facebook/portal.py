5826e44a844f9ed03dbb9b4a47a654c88930340f
Carl Ambroselli <git@carl-ambroselli.de>
2022-02-19 20:06:20 +0100

    Don't crash when no message are in the matrix room

--- mautrix_facebook/portal.py
+++ mautrix_facebook/portal.py
@@ -1812,7 +1812,7 @@ class Portal(DBPortal, BasePortal):
         most_recent = await DBMessage.get_most_recent(self.fbid, self.fb_receiver)
         if most_recent and is_initial:
             self.log.debug("Not backfilling %s: already bridged messages found", self.fbid_log)
-        elif last_active and most_recent.timestamp >= last_active:
+        elif last_active and most_recent != None and most_recent.timestamp >= last_active:
             self.log.debug(
                 "Not backfilling %s: last activity is equal to most recent bridged "
                 "message (%s >= %s)",
